e3de51cf75c62421a52cf129e30e067b3e240dc8
Lucas Mendes <lms252006@gmail.com>
2025-06-23 20:52:51 -0300

    pensamento para teste

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -7,6 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
+  listaPensamentos = [
+  {
+    conteudo: 'Passo informações para o componente filho',
+    autoria: 'Componente pai',
+    modelo: 'modelo3'
+  },
+  {
+    conteudo: 'Minha propriedade é decorada com @Input()',
+    autoria: 'Angular',
+    modelo: 'modelo2'
+  }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
--- src/app/components/pensamentos/pensamento/pensamento.component.ts
+++ src/app/components/pensamentos/pensamento/pensamento.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pensamento',
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PensamentoComponent implements OnInit {
 
+  @Input() pensamento =
+  {
+    conteudo: 'I love Angular',
+    autoria: 'Dev',
+    modelo: 'modelo3'
+  }
   constructor() { }
 
   ngOnInit(): void {
