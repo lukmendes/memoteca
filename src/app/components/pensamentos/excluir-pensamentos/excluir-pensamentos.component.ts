ead931a614b2695cefe01efe87a0ec686594c292
Lucas Mendes <lms252006@gmail.com>
2025-06-25 19:23:43 -0300

    métodos (excluir e cancelar pensamentos), constructor, estrutura de pensamentos

--- src/app/components/pensamentos/excluir-pensamentos/excluir-pensamentos.component.ts
+++ src/app/components/pensamentos/excluir-pensamentos/excluir-pensamentos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-excluir-pensamentos',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExcluirPensamentosComponent implements OnInit {
 
-  constructor() { }
+  pensamento: Pensamento = {
+    id: 0,
+    conteudo: '',
+    autoria: '',
+    modelo: ''
+  }
+
+  constructor(private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => this.pensamento = pensamento)
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id).subscribe(() => {
+        this.router.navigate(['/listarPensamento'])
+      })
+    }
+  }
+
+  cancelarPensamento() {
+    this.router.navigate(['/listarPensamento']);
   }
 
 }
