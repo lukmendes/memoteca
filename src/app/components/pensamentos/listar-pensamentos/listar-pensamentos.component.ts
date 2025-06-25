8572e257e1df8ca2ff5924d46f6edca8617f9d26
Lucas Mendes <lms252006@gmail.com>
2025-06-25 14:02:39 -0300

    importando interface Pensamento

--- src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
+++ src/app/components/pensamentos/listar-pensamentos/listar-pensamentos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
 
 @Component({
   selector: 'app-listar-pensamentos',
@@ -7,23 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListarPensamentosComponent implements OnInit {
 
-  listaPensamentos = [
-  {
-    conteudo: 'Passo informações para o componente filho',
-    autoria: 'Componente pai',
-    modelo: 'modelo3'
-  },
-  {
-    conteudo: 'Minha propriedade é decorada com @Input()',
-    autoria: 'Componente filho',
-    modelo: 'modelo2'
-  },
-  {
-    conteudo: 'Morbi a metus. Phasellus enim erat, vestibulum vel, aliquam a, posuere eu, velit. Nullam sapien sem, ornare ac, nonummy non, lobortis a, enim. Nunc tincidunt ante vitae massa. Duis ante orci, molestie vitae, vehicula venenatis, tincidunt ac, pede. Nulla accumsan, elit sit amet varius semper, nulla mauris mollis quam, tempor suscipit diam nulla vel leo. Etiam commodo dui eget wisi. Donec iaculis',
-    autoria: 'Lorem Ipsum',
-    modelo: 'modelo1'
-  }
-  ];
+  listaPensamentos: Pensamento[] = [];
 
   constructor() { }
 
