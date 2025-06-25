59826a29f0ed24fe4a81bc9bab14c178d798ed93
Lucas Mendes <lms252006@gmail.com>
2025-06-25 18:43:25 -0300

    novo component

---
+++ src/app/components/pensamentos/excluir-pensamentos/excluir-pensamentos.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-excluir-pensamentos',
+  templateUrl: './excluir-pensamentos.component.html',
+  styleUrls: ['./excluir-pensamentos.component.css']
+})
+export class ExcluirPensamentosComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
